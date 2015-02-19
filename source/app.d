1b45fcc62d6a90062c6533fde407ff63113d1419
Josh Hofing <joshhofing@gmail.com>
2015-02-18 21:07:38 -0500

    initial gravity simulation

--- source/app.d
+++ source/app.d
@@ -1,7 +1,13 @@
 import std.stdio;
 import std.typecons;
+import std.conv;
+import std.random;
+
+import core.simd;
 
 import gfm.sdl2;
+
+import gravitas.world;
 
 void main() {
     int width = 1000;
@@ -19,8 +25,27 @@ void main() {
     auto renderer = scoped!SDL2Renderer(window);
 
     bool running = true;
+    World world;
+    foreach (i; 0 .. 1000) {
+        world.push_ent(uniform(0, width), uniform(0, height), 0, 0, 0, 0, uniform(1, 1000));
+    }
     while (running) {
         sdl2.processEvents();
+
+        renderer.setColor(255, 255, 255);
+        foreach (i; 0 .. world.last_ent) {
+            auto x = world.xs[i];
+            auto y = world.ys[i];
+            if (0 < x && x < width &&
+                0 < y && y < height) {
+                renderer.drawPoint(world.xs[i].to!int, world.ys[i].to!int);
+            }
+        }
+        world.step(.01);
+
+        renderer.present();
+        renderer.setColor(0, 0, 0, 0);
+        renderer.clear();
 
         // Exit on escape
         if (sdl2.keyboard().isPressed(SDLK_ESCAPE)) {
---
+++ source/world.d
@@ -0,0 +1,55 @@
+module gravitas.world;
+
+import std.math;
+
+import core.simd;
+
+struct World {
+    static const int n_ents = 1024;
+    int last_ent = 0;
+    float[n_ents] xs;
+    float[n_ents] ys;
+    float[n_ents] vxs;
+    float[n_ents] vys;
+    float[n_ents] axs;
+    float[n_ents] ays;
+    float[n_ents] masses;
+
+    void step(float timestep = 1.0) {
+        // Step everything
+        foreach (i; 0 .. last_ent) {
+            foreach (o; 0 .. last_ent) {
+                auto dx = xs[o] - xs[i];
+                auto dy = ys[o] - ys[i];
+                auto len = sqrt(dx * dx + dy * dy);
+                if (len != 0) {
+                    auto dirx = dx / len;
+                    auto diry = dy / len;
+                    axs[i] += dirx * (masses[i] * masses[o]) / (len * len);
+                    ays[i] += diry * (masses[i] * masses[o]) / (len * len);
+                    if (axs[i] > 10000) axs[i] = 10000;
+                    if (ays[i] > 10000) ays[i] = 10000;
+                }
+            }
+            vxs[i] += axs[i] * timestep;
+            vys[i] += ays[i] * timestep;
+            xs[i] += vxs[i] * timestep;
+            ys[i] += vys[i] * timestep;
+        }
+    }
+
+    void push_ent(
+            float x, float y,
+            float vx, float vy,
+            float ax, float ay,
+            float mass) {
+        this.xs[last_ent] = x;
+        this.ys[last_ent] = y;
+        this.vxs[last_ent] = vx;
+        this.vys[last_ent] = vy;
+        this.axs[last_ent] = ax;
+        this.ays[last_ent] = ay;
+        this.masses[last_ent] = mass;
+        ++last_ent;
+    }
+}
