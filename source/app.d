0417775c2d07d7c3d602ebbf0678b1e1bd10b83c
Josh Hofing <joshhofing@gmail.com>
2015-02-18 20:10:04 -0500

    Most basic of everything

--- source/app.d
+++ source/app.d
@@ -1,6 +1,30 @@
 import std.stdio;
+import std.typecons;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+import gfm.sdl2;
+
+void main() {
+    int width = 1000;
+    int height = 1000;
+
+    // load dynamic libraries
+    auto sdl2 = scoped!SDL2(null);
+
+    // create an OpenGL-enabled SDL window
+    auto window = scoped!SDL2Window(sdl2, 
+                                    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                    width, height,
+                                    SDL_WINDOW_BORDERLESS);
+
+    auto renderer = scoped!SDL2Renderer(window);
+
+    bool running = true;
+    while (running) {
+        sdl2.processEvents();
+
+        // Exit on escape
+        if (sdl2.keyboard().isPressed(SDLK_ESCAPE)) {
+            running = false;
+        }
+    }
 }
