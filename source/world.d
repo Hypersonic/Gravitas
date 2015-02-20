9b433c4a2614a53910023d6d61b1afda8155f634
Josh Hofing <joshhofing@gmail.com>
2015-02-20 15:17:53 -0500

    Add Z axis

--- source/world.d
+++ source/world.d
@@ -17,10 +17,13 @@ struct World {
     int last_ent_sub = 0;
     vecType[n_ents / veclen] xs;
     vecType[n_ents / veclen] ys;
+    vecType[n_ents / veclen] zs;
     vecType[n_ents / veclen] vxs;
     vecType[n_ents / veclen] vys;
+    vecType[n_ents / veclen] vzs;
     vecType[n_ents / veclen] axs;
     vecType[n_ents / veclen] ays;
+    vecType[n_ents / veclen] azs;
     vecType[n_ents / veclen] masses;
 
     void step(float timestep = 1.0) {
@@ -28,6 +31,7 @@ struct World {
         foreach (i; iota(0, last_ent + 1).parallel) {
             axs[i] = 0;
             ays[i] = 0;
+            azs[i] = 0;
             foreach (o; 0 .. last_ent + 1) {
                 foreach (j; iota(0, veclen)) {
                     foreach(k; iota(0, veclen)) {
@@ -35,12 +39,11 @@ struct World {
                         import std.stdio;
                         auto dx = xs[o].array[k] - xs[i].array[j];
                         auto dy = ys[o].array[k] - ys[i].array[j];
+                        auto dz = zs[o].array[k] - zs[i].array[j];
 
-                        //writeln(ys[o].array[k]);
-                        //writeln(dx, dy);
                         // compute lengths
                         auto len = 0f;
-                        auto sqsum = dx * dx + dy * dy;
+                        auto sqsum = dx * dx + dy * dy + dz * dz;
                         len = sqrt(sqsum);
 
                         auto maxdist = masses[o].array[k] / 8;
@@ -57,6 +60,7 @@ struct World {
                             auto mass_ratio = masses[smaller_f].array[smaller_s] / masses[bigger_f].array[bigger_s];
                             vxs[bigger_f].array[bigger_s] += vxs[smaller_f].array[smaller_s] * mass_ratio;
                             vys[bigger_f].array[bigger_s] += vys[smaller_f].array[smaller_s] * mass_ratio;
+                            vzs[bigger_f].array[bigger_s] += vzs[smaller_f].array[smaller_s] * mass_ratio;
 
                             // Move mass from smaller to bigger
                             masses[bigger_f].array[bigger_s] += masses[smaller_f].array[smaller_s];
@@ -65,15 +69,20 @@ struct World {
                             // Get rid of smaller
                             xs[smaller_f].array[smaller_s] = -100000000;
                             ys[smaller_f].array[smaller_s] = -100000000;
+                            zs[smaller_f].array[smaller_s] = -100000000;
                             vxs[smaller_f].array[smaller_s] = 0;
                             vys[smaller_f].array[smaller_s] = 0;
+                            vzs[smaller_f].array[smaller_s] = 0;
                             axs[smaller_f].array[smaller_s] = 0;
                             ays[smaller_f].array[smaller_s] = 0;
+                            azs[smaller_f].array[smaller_s] = 0;
                         } else if (len != 0) {
                             auto dirx = dx / len;
                             auto diry = dy / len;
+                            auto dirz = dz / len;
                             axs[i].array[j] += dirx * G * (masses[o].array[k]) / (len * len);
                             ays[i].array[j] += diry * G * (masses[o].array[k]) / (len * len);
+                            azs[i].array[j] += dirz * G * (masses[o].array[k]) / (len * len);
                         }
                     }
                 }
@@ -84,8 +93,10 @@ struct World {
             }
             vxs[i] += axs[i] * ts;
             vys[i] += ays[i] * ts;
+            vzs[i] += azs[i] * ts;
             xs[i] += vxs[i] * ts;
             ys[i] += vys[i] * ts;
+            zs[i] += vzs[i] * ts;
         }
     }
 
@@ -96,10 +107,13 @@ struct World {
             float mass) {
         this.xs[last_ent].array[last_ent_sub] = x;
         this.ys[last_ent].array[last_ent_sub] = y;
+        this.zs[last_ent].array[last_ent_sub] = 0;
         this.vxs[last_ent].array[last_ent_sub] = vx;
         this.vys[last_ent].array[last_ent_sub] = vy;
+        this.vzs[last_ent].array[last_ent_sub] = 0;
         this.axs[last_ent].array[last_ent_sub] = ax;
         this.ays[last_ent].array[last_ent_sub] = ay;
+        this.azs[last_ent].array[last_ent_sub] = 0;
         this.masses[last_ent].array[last_ent_sub] = mass;
         ++last_ent_sub;
         if (last_ent_sub == veclen) {
@@ -107,10 +121,13 @@ struct World {
             ++last_ent;
             this.xs[last_ent] = 0;
             this.ys[last_ent] = 0;
+            this.zs[last_ent] = 0;
             this.vxs[last_ent] = 0;
             this.vys[last_ent] = 0;
+            this.vzs[last_ent] = 0;
             this.axs[last_ent] = 0;
             this.ays[last_ent] = 0;
+            this.azs[last_ent] = 0;
             this.masses[last_ent] = 0;
         }
     }
