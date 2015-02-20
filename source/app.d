7d87cc775953df3b40aca4bb4a6abac044c5b347
Josh Hofing <joshhofing@gmail.com>
2015-02-20 15:35:59 -0500

    Allow toggling of velocity and acceleration vectors

--- source/app.d
+++ source/app.d
@@ -26,42 +26,51 @@ void main() {
 
     auto renderer = scoped!SDL2Renderer(window);
 
+    bool draw_vel = true;
+    bool draw_acc = true;
     bool running = true;
     World world;
-    foreach (i; 0 .. 128) {
-        world.push_ent(uniform(0, width), uniform(0, height), uniform(0, depth), 0, 0, 0, 0, 0, 0, uniform(1, 20));
+    foreach (i; 0 .. 1024) {
+        auto vx = uniform(-10, 10);
+        auto vy = uniform(-10, 10);
+        auto vz = uniform(-10, 10);
+        world.push_ent(uniform(0, width), uniform(0, height), uniform(0, depth), vx, vy, vz, 0, 0, 0, uniform(1, 20));
     }
     while (running) {
         sdl2.processEvents();
 
-        // Draw accelerations
-        renderer.setColor(128, 0, 0);
-        foreach (i; 0 .. world.last_ent + 1) {
-            foreach (k; 0 .. world.veclen) {
-                auto x = world.xs[i].array[k] + world.vxs[i].array[k] + world.axs[i].array[k];
-                auto y = world.ys[i].array[k] + world.vys[i].array[k] + world.ays[i].array[k];
-                if (0 < x && x < width &&
-                        0 < y && y < height) {
-                    renderer.drawLine(
-                            (world.xs[i].array[k] + world.vxs[i].array[k]).to!int,
-                            (world.ys[i].array[k] + world.vys[i].array[k]).to!int,
-                            x.to!int, y.to!int);
+        if (draw_acc) {
+            // Draw accelerations
+            renderer.setColor(128, 0, 0);
+            foreach (i; 0 .. world.last_ent + 1) {
+                foreach (k; 0 .. world.veclen) {
+                    auto x = world.xs[i].array[k] + world.vxs[i].array[k] + world.axs[i].array[k];
+                    auto y = world.ys[i].array[k] + world.vys[i].array[k] + world.ays[i].array[k];
+                    if (0 < x && x < width &&
+                            0 < y && y < height) {
+                        renderer.drawLine(
+                                (world.xs[i].array[k] + world.vxs[i].array[k]).to!int,
+                                (world.ys[i].array[k] + world.vys[i].array[k]).to!int,
+                                x.to!int, y.to!int);
+                    }
                 }
             }
         }
 
-        // Draw velocities
-        renderer.setColor(0, 0, 128);
-        foreach (i; 0 .. world.last_ent + 1) {
-            foreach (k; 0 .. world.veclen) {
-                auto x = world.xs[i].array[k] + world.vxs[i].array[k];
-                auto y = world.ys[i].array[k] + world.vys[i].array[k];
-                if (0 < x && x < width &&
-                        0 < y && y < height) {
-                    renderer.drawLine(
-                            world.xs[i].array[k].to!int,
-                            world.ys[i].array[k].to!int,
-                            x.to!int, y.to!int);
+        if (draw_vel) {
+            // Draw velocities
+            renderer.setColor(0, 0, 128);
+            foreach (i; 0 .. world.last_ent + 1) {
+                foreach (k; 0 .. world.veclen) {
+                    auto x = world.xs[i].array[k] + world.vxs[i].array[k];
+                    auto y = world.ys[i].array[k] + world.vys[i].array[k];
+                    if (0 < x && x < width &&
+                            0 < y && y < height) {
+                        renderer.drawLine(
+                                world.xs[i].array[k].to!int,
+                                world.ys[i].array[k].to!int,
+                                x.to!int, y.to!int);
+                    }
                 }
             }
         }
@@ -88,6 +97,14 @@ void main() {
         // Exit on escape
         if (sdl2.keyboard().isPressed(SDLK_ESCAPE)) {
             running = false;
+        }
+        // Hit a to toggle acceleration drawing
+        if (sdl2.keyboard().isPressed(SDLK_a)) {
+            draw_acc = !draw_acc;
+        }
+        // Hit v to toggle velocity drawing
+        if (sdl2.keyboard().isPressed(SDLK_v)) {
+            draw_vel = !draw_vel;
         }
     }
 }
